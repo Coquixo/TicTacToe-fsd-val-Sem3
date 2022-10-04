4d274a01bd879d1f8510c7ccfb41531fc0661bb9
Alex López <coquixo98@gmail.com>
2022-10-04 23:30:55 +0200

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+function Start() {
+
+
+}
+
+function NewTable() {
+
+
+
+}
+
+function NameSwap() {
+    // let name = prompt();
+    return name;
+}
+
+function ChooseMode() {
+
+    // humano-humano / humano-cpu / cpu-humano
+}
+
+function ValidPosition() {
+
+
+}
+
+// -------------------------------------------------- Functions----------------------------------------------
+
+NameSwap();
+
+
+
+
+
+//---------------------------------------------------Functions Inst------------------------------------------
+
+
+
+
+
+
+for (let i = []; i.length <= 3; i++) {
+    for (let j = []; j <= i.length; j++);
+
+    console.log(i[j]);
+}
